fd39a7a6e4d4ab90005298073a5060dde2410efc
Ali <ali>
2020-07-07 14:08:06 -0500

    Initial Commit

--- flashLightApp/ViewController.swift
+++ flashLightApp/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var topLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    @IBAction func onButtonTap(_ sender: UIButton) {
+        self.view.backgroundColor = UIColor.blue
+        
+        topLabel.text = "It's not easy being green"
+        topLabel.textColor = UIColor.green
+        
+        
+    }
+    
+    @IBAction func onSecondButtonTap(_ sender: UIButton) {
+        topLabel.text = "What color am I?"
+        self.view.backgroundColor = UIColor.white
+    }
+    
+    @IBAction func thirdButton(_ sender: UIButton) {
+        var redV = CGFloat(drand48())
+        var greenV = CGFloat(drand48())
+        var blueV = CGFloat(drand48())
+        
+        self.view.backgroundColor = UIColor(red: redV, green: greenV, blue: blueV, alpha: 0.5)
+    }
+    
+    override func becomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        var redV = CGFloat(drand48())
+        var greenV = CGFloat(drand48())
+        var blueV = CGFloat(drand48())
+        
+        if motion == .motionShake {
+            self.view.backgroundColor = UIColor(red: redV, green: greenV, blue: blueV, alpha: 0.5)
+        }
+    }
+    
 }
 
